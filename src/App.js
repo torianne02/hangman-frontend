b837258d86129719e6b1f35384e09e4a5da5324e
torianne02 <victoria.fluharty@gmail.com>
2019-10-17 13:43:18 -0700

    removes image from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import hangman from './images/hangman.png'
 import './App.css';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <div className="App">
-      <img src={hangman} alt="hangman logo" />
+      <NavBar />
     </div>
   );
 }
