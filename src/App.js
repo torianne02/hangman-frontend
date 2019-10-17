05d18b5a836a4d13f54554cea71c7f13da33bbf2
torianne02 <victoria.fluharty@gmail.com>
2019-10-17 13:49:50 -0700

    imports BrowserRouter as Router from react-router-dom in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import hangman from './images/hangman.png'
 import './App.css';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+      </div>
+    </Router>
   );
 }
 
