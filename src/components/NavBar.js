bd8b0bdd0beb201890880ec6f4f4ef8077583448
torianne02 <victoria.fluharty@gmail.com>
2019-10-17 13:42:56 -0700

    adds image as NavbarBrand

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
+import { 
+  Navbar, 
+  NavbarBrand, 
+  NavItem, 
+  NavLink, 
+  Nav 
+} from 'reactstrap';
+import hangman from '../images/hangman.png';
 
 const NavBar = () => {
   return (
     <Navbar>
       <NavbarBrand href="/">
-        The Hangman Game
+        <img src={hangman} alt="hangman logo" />
       </NavbarBrand>
     </Navbar>
   );
